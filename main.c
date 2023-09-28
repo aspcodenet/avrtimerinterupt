a945411bb51c5f1ee1244651c2191ee29c8a4eb2
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-09-28 16:03:16 +0200

    aaa

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
 
 #define BIT_SET(a, b) ((a) |= (1ULL << (b)))
 #define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
@@ -16,43 +16,28 @@ void checkAndHandleInput(){
     //Kolla o trtckt på knapp eller whatever
 }
 
-// int main(){
-
-//     BIT_SET(DDRB, LED_PIN); //Sätt led_pin till output mode
-//     //TCCR0A – Timer/Counter Control Register A
-//     TCCR0B |=  (1 << CS02)|(1 << CS00); //Prescaler 1024
-//     TCNT0 = 0;
-//     int overflows = 0;
-
-//     while(1){
-        
-//         //Check input
-//         checkAndHandleInput();
-//         //
-//         if(TCNT0 == 255){
-//             overflows++;
-//             TCNT0 = 0; 
-//         }
-//         if (overflows == 61)
-//         {
-//             BIT_FLIP(PORTB,LED_PIN);
-//             overflows = 0;
-//             TCNT0 = 0;            // reset counter
-//         }        
- 
-//     }
-
-//     return 0;
-// }
-
-
+ISR(TIMER1_OVF_vect){
+    BIT_FLIP(PORTB,LED_PIN);
+    TCNT1 = 65535-15625;
+}
 
 //I dont clear it - CTC
 int main(){
 
     BIT_SET(DDRB, LED_PIN); //Sätt led_pin till output mode
+
+    //Max value of 65535 -> overflow -> ISR
+    TCNT1 = 65535-15625; // Start value every cycle ++
+    //16Mhz / 1024 = 15625 Hz  - ticks per sec
+    TCCR1B = (1 << CS10)| (1 << CS12); // 1024
+
     //TCCR0A – Timer/Counter Control Register A
     TCCR0B |=  (1 << CS02)|(1 << CS00); //Prescaler 1024
+    TCCR1A = 0;
+    TIMSK1 = (1 << TOIE1);
+
+    sei();
+
     TCNT0 = 0;
     int overflows = 0;
 
@@ -60,17 +45,6 @@ int main(){
         
         //Check input
         checkAndHandleInput();
-        //
-        if(TCNT0 == 255){
-            overflows++;
-            TCNT0 = 0; 
-        }
-        if (overflows == 61)
-        {
-            BIT_FLIP(PORTB,LED_PIN);
-            overflows = 0;
-            TCNT0 = 0;            // reset counter
-        }        
  
     }
 
